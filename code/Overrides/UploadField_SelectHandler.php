2592e85f72e7fffc2c26847f55fc77ccab5afcc8
Mellisa Hankins <mell@milkywaymultimedia.com.au>
2014-12-09 18:36:12 +1100

    FEATURE: Ability to override the type of objects in the UploadField_SelectHandler GridField, and also added ability to extend it if required.

---
+++ code/Overrides/UploadField_SelectHandler.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Milkyway Multimedia
+ * UploadField_SelectHandler.php
+ *
+ * @package milkywaymultimedia.com.au
+ * @author Mellisa Hankins <mell@milkywaymultimedia.com.au>
+ */
+
+namespace Milkyway\SS\Overrides;
+
+class UploadField_SelectHandler extends \UploadField_SelectHandler {
+	protected function getListField($folderID) {
+		$field = parent::getListField($folderID);
+
+		if($this->parent->getConfig('listDataClass')) {
+			if($files = $field->fieldByName('Files'))
+				$files->setList(\DataList::create($this->parent->getConfig('listDataClass'))->filter('ParentID', $folderID));
+		}
+
+		parent::extend('updateListField', $field, $folderID);
+
+		return $field;
+	}
+} 
