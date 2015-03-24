19d2e6e7ba40e5a94c26bb3a28b4a586dc12ecc3
Mellisa Hankins <mell@milkywaymultimedia.com.au>
2015-03-25 01:41:40 +1100

    Added firstOrMake (from Laravel)

--- code/extensions/DataObject.php
+++ code/extensions/DataObject.php
@@ -3,11 +3,28 @@
  * Milkyway Multimedia
  * DataObject.php
  *
- * @package milkyway-multimedia/silverstripe-mwm
+ * @package milkyway-multimedia/ss-mwm
  * @author Mellisa Hankins <mell@milkywaymultimedia.com.au>
  */
 class DataObject extends \DataExtension {
     function i18n_description() {
         return _t(get_class($this->owner).'.DESCRIPTION', $this->owner->config()->description);
+    }
+
+    public function firstOrMake($filter = [], $additionalData = [], $write = true) {
+        if (!($record = $this->owner->get()->filter($filter)->first())) {
+            $record = $this->owner->create(array_merge($filter, $additionalData));
+
+            if($write) {
+                $record->write();
+                $record->isNew = true;
+            }
+        }
+
+        return $record;
+    }
+
+    public function firstOrCreate($filter = [], $additionalData = [], $write = true) {
+        return $this->owner->firstOrMake($filter, $additionalData, $write);
     }
 } 
